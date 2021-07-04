6861fb492394573246325a5b0318c87d10504189
MaryGitLive <86549011+MaryGitLive@users.noreply.github.com>
2021-07-04 08:02:28 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -3,5 +3,6 @@ public class helloworldjavatest{
 	public static void main(String[] args){
 		
 		System.out.println("Hello World - polling test by jenkins from StarAgile");
+		System.out.println("Check Poll");
 	}
 }
